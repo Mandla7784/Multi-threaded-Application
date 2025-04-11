f29788dd2e3e964ebed8fa21346fa0e7852523a6
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2025-04-11 11:38:40 +0200

    Added threading

--- src/main/java/Registration.java
+++ src/main/java/Registration.java
@@ -1,10 +1,11 @@
 package src.main.java;
 
-public class Registration {
+public class Registration extends Thread {
     private  int spots = 6;
     private Object lock = new Object();
+    private String name;
 
-
+ 
     public void register(String studentName){
          // holding the lock 
          synchronized(lock){
@@ -15,12 +16,28 @@ public class Registration {
 
              }else {
                  System.out.println(studentName + "tried to register but no spot were left");
-
              }
          }
-
+    }
+    public int  getSPotCount() {
+        return spots;
+   
     }
 
+  @Override
+  public void run(){
+      for(int i = 0 ; i < spots ; i ++){
+        System.out.println(name + "has started ");
+        try {
+            
+            Thread.sleep(600);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
 
+        }
+      }
+      System.out.println("Finished registration...");
+
+  }
     
 }

abe7aaf51fe664f96d27fe1ff5f2a72d6fc6b260
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2025-04-11 11:53:40 +0200

    Done

--- src/main/java/MultiThreadApplication.java
+++ src/main/java/MultiThreadApplication.java
@@ -2,16 +2,31 @@ package src.main.java;
 
 class MultiThreadApplication extends Thread{
     public static void main(String [] rgs){
-       Registration registrstion  = new Registration();
+       Registration registration = new Registration("Registraion");
+        
 
+       Thread student_1 = new Thread(() -> registration.register("Mandla"));
+       Thread student_2 = new Thread(() -> registration.register("Benjamin"));
+       Thread student_3 = new Thread(() -> registration.register("Kilo"));
+       Thread student_4 = new Thread(() -> registration.register("Gang"));
 
-       Thread student_1 = new Thread(()-> registrstion.register("Mandla"));
-       Thread student_2 = new Thread(()-> registrstion.register("Benjamin"));
-       Thread student_3 =  new Thread(()-> registrstion.register("Kilo"));
-       Thread student_4 = new Thread(()-> registrstion.register("Gang"));
-       
+       student_1.start();
+       student_2.start();
+       student_3.start();
+       student_4.start();
 
+       // You might want to wait for these threads to finish
+       try {
+           student_1.join();
+           student_2.join();
+           student_3.join();
+           student_4.join();
+       } catch (InterruptedException e) {
+           e.printStackTrace();
+       }
 
+       System.out.println("Final spot count: " + registration.getSPotCount());
+       System.out.println("Registered students: " + registration.spaces);
     
 
     }
--- src/main/java/Registration.java
+++ src/main/java/Registration.java
@@ -3,46 +3,46 @@ package src.main.java;
 import java.util.ArrayList;
 
 public class Registration extends Thread {
-    private  int spots = 6;
+    private int spots = 6;
     ArrayList<String> spaces = new ArrayList<>();
-
     private Object lock = new Object();
     private String name;
 
- 
-    public void register(String studentName){
-         // holding the lock 
-         synchronized(lock){
-             if(spots > 0){
-                 System.out.println(studentName + "is registering"+ "There are " + spots + "left");
-                   spaces.add(studentName);
-                 spots--;
-                 System.out.println(studentName + "Registered");
 
-             }else {
-                 System.out.println(studentName + "tried to register but no spot were left");
-             }
-         }
-    }
-    public int  getSPotCount() {
-        return spots;
-   
+    public Registration(String name) {
+        this.name = name;
     }
 
-  @Override
-  public void run(){
-      for(int i = 0 ; i < spots ; i ++){
-        System.out.println(name + "has started ");
-        try {
-            
-            Thread.sleep(600);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-
+    public void register(String studentName) {
+        synchronized (lock) {
+            if (spots > 0) {
+                System.out.println(studentName + " is registering. There are " + spots + " left");
+                spaces.add(studentName);
+                spots--;
+                System.out.println(studentName + " Registered");
+            } else {
+                System.out.println(studentName + " tried to register but no spots were left");
+            }
         }
-      }
-      System.out.println("Finished registration...");
+    }
 
-  }
-    
+    public int getSPotCount() {
+        return spots;
+    }
+
+    @Override
+    public void run() {
+        register(this.name); 
+
+ 
+        for (int i = 0; i < 3; i++) { 
+            System.out.println(this.name + " has started some post-registration activity.");
+            try {
+                Thread.sleep(600);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(this.name + " finished post-registration activity.");
+    }
 }
